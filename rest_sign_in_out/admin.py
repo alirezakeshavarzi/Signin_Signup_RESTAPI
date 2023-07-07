66e8c6432574fd8903c55d3391a48cfedded67d6
Alireza.k <ahmidyly@gmail.com>
2023-07-07 15:26:15 +0330

    add new sections of User model

--- rest_sign_in_out/admin.py
+++ rest_sign_in_out/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import User
 
 class UserClass(admin.ModelAdmin):
-    list_display = ['id', 'fname', 'lname']
+    list_display = ['id', 'fname', 'lname', 'username', 'email', 'phone','password']
 
 admin.site.register(User, UserClass)
 # Register your models here.
