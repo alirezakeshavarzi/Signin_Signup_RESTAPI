e1c69a398489030d8b8ee7e80ae60a744d0442bc
Alireza.k <ahmidyly@gmail.com>
2026-09-19 17:23:35 +0330

    User bug fix

--- rest_sign_in_out/models.py
+++ rest_sign_in_out/models.py
@@ -1,12 +1,9 @@
+
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
-class User(models.Model):
-    fname = models.CharField(max_length=20, null=True)
-    lname = models.CharField(max_length=20, null=True)
-    username = models.CharField(max_length=20, unique=True)
-    email = models.CharField(max_length=50, unique=True, null=True)
+class User(AbstractUser):
     phone = models.IntegerField(null=True)
-    password = models.CharField(max_length=51)
-
 
 # Create your models here.
