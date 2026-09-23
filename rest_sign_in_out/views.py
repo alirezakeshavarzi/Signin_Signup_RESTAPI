9fe0a5716877c9953cbef3de5fc1541039ca0ed4
Alireza.k <ahmidyly@gmail.com>
2026-09-23 16:17:15 +0330

    create new serializers

--- rest_sign_in_out/views.py
+++ rest_sign_in_out/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 
-from .seri import UserRegisterSerializer, UserInfoSerializer
+from .serializers import UserRegisterSerializer, UserInfoSerializer
 
 
 
